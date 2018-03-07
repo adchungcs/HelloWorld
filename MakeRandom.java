5cd6ebd0647050789e7e359c1a6e89c5fbe97e8d
Alex767 <alch888@gmail.com>
2018-03-07 13:42:09 -0500

    Added a program to generate 100 random numbers

---
+++ MakeRandom.java
@@ -0,0 +1,9 @@
+import java.util.Random;		
+public class MakeRandom{
+	public static void main(String[] args){
+		Random random = new Random();
+		for(int i = 0; i < 100; i++){
+			System.out.println(random.nextInt(100));
+		}	
+	}
+}
