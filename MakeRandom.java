2dfb5de43e18bb8a9d71e6b7536be513c554b7e7
adchungcs <37147761+adchungcs@users.noreply.github.com>
2018-03-07 13:47:32 -0500

    Fixed typo

--- MakeRandom.java
+++ MakeRandom.java
@@ -3,7 +3,7 @@ public class MakeRandom{
 	public static void main(String[] args){
 		Random random = new Random();
 		for(int i = 0; i < 100; i++){
-			System.out.println("#" + i + random.nextInt(100));
+			System.out.println("#" + i + " " + random.nextInt(100));
 		}	
 	}
 }
