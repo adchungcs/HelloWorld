eb06e8dcec266c80cf5bc744548e089d380e77a7
Alex767 <alch888@gmail.com>
2018-03-07 13:49:03 -0500

    Added new iteration feature

--- MakeRandom.java
+++ MakeRandom.java
@@ -3,7 +3,8 @@ public class MakeRandom{
 	public static void main(String[] args){
 		Random random = new Random();
 		for(int i = 0; i < 100; i++){
-			System.out.println(random.nextInt(100));
+			System.out.println(i);
+			System.out.println(random.nextInt(100);
 		}	
 	}
 }
